4e184d4749982792586d1531616122daf093d6bf
Alumnobyjus1 <104869929+Alumnobyjus1@users.noreply.github.com>
2022-09-24 12:42:48 -0500

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+jugador1=localStorage.getItem("jugador_1");
+jugador2=localStorage.getItem("jugador_2");
+player1_score=0;
+player2_score=0;
+document.getElementById("player_1").innerHTML=jugador1+": ";
+document.getElementById("player_2").innerHTML=jugador2+": ";
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score");innerHTML=player2_score;
+document.getElementById("player_question").innerHTML="turno para preguntar: "+jugador1;
+document.getElementById("player_answer").innerHTML="turno para responder: "+jugador2;              
